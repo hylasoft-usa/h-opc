eb55e395b2cbabfdee6303c4e158d14e05ea9a4b
jmbeach <jaredbeachdesign@gmail.com>
2016-08-05 08:54:24 -0500

    Recreate does not work as expected either
    I changed the reconnect test to be extremely obvious as to why it does not work, you should get an error if you never successfully connected before and then you reconnect, but you don't because of the condition inside reconnect to only reconnect if connected. As I said before, I think that reconnect should try to reconnect regardless of whether or not you are currently connected. In fact, if I were to put a condition in there, I would make it return if it were already connected, not if it wasn't. Also, the reconnect and recreate functions should not modify the status property because that is set in the keepalive function.
    
    The Recreate function has the exact same problem so the test fails when you call recreate because the old session is the exact same object as the new session.

--- tests/UaTest.cs
+++ tests/UaTest.cs
@@ -10,6 +10,7 @@ using System.Configuration;
 using Opc.Ua.Client;
 using Opc.Ua;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Hylasoft.Opc.Tests
 {
@@ -194,16 +195,29 @@ namespace Hylasoft.Opc.Tests
     }
 
     [Test]
-    public void UaTestKeepAliveReConnect()
+    public void UaTestReConnect()
+    {
+      var client = new TestExtendUaClient(new Uri(ConfigurationManager.AppSettings["UATestEndpoint"]));
+      /* Should throw error because session should not be
+       * initialized without calling connect */
+      Assert.Throws<System.NullReferenceException>(() => client.ReConnect());
+    }
+
+    [Test]
+    public void UaTestSessionRecreate()
     {
       var client = new TestExtendUaClient(new Uri(ConfigurationManager.AppSettings["UATestEndpoint"]));
       client.Connect();
       var i = 0;
+      Session oldSession = null;
       client.ServerConnectionLost += (object sender, EventArgs e) =>
       {
         i++;
         Assert.AreEqual(OpcStatus.NotConnected, client.Status);
-        client.ReConnect();
+        // store the session to make sure a new one is created
+        oldSession = client.SessionExtended;
+        client.RecreateSession();
+        // put server back in good working order
         client.SessionExtended.OperationTimeout = 200;
         client.SessionExtended.KeepAliveInterval = 100;
       };
@@ -211,9 +225,9 @@ namespace Hylasoft.Opc.Tests
       client.SessionExtended.KeepAliveInterval = 10;
       Thread.Sleep(100);
       Assert.Greater(i, 0);
-      // Give some time to call reconnect
+      // Give some time to call recreate
       Thread.Sleep(100);
-      Assert.AreEqual(OpcStatus.Connected, client.Status);
+      Assert.AreNotSame(oldSession, client.SessionExtended);
     }
   }
 }
