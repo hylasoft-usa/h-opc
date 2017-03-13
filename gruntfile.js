a6c8507f17be33a385d64bddbb85b544f2c953ca
Jared Beach <j.beach@hylasoftusa.com>
2017-03-12 20:59:53 -0500

    fix appveyor

--- gruntfile.js
+++ gruntfile.js
@@ -77,7 +77,7 @@ module.exports = function(grunt) {
     nugetrestore: {
       restore: {
         src: '<%= srcPath %><%= solutionName %>',
-        dest: 'packages/'
+        dest: 'packages/',
 				options: {
 					msbuildversion: 12
 				}
